4cedc3ca975929ed45312b5b0dcf2258fea476c9
Josh Mclain <josh.mclain@oneorigin.us>
2024-03-18 21:55:15 -0700

    :beers: (workflow) added auto mated code review

--- scripts/check-branch-name.js
+++ scripts/check-branch-name.js
@@ -5,8 +5,8 @@ const { execSync } = require('child_process');
 const branchName = execSync('git branch --show-current').toString().trim();
 
 if (!/^JIRA-\d{1,5}/.test(branchName)) {
-    console.error(
-        'ERROR: Branch names must start with "SMSDEV-{XXX}" and end with Jira ticket number.',
-    );
-    process.exit(1);
+  console.error(
+    'ERROR: Branch names must start with "SMSDEV-{XXX}" and end with Jira ticket number.',
+  );
+  process.exit(1);
 }
